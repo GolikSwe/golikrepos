9153c43e7f95089d3dd3a49f1e38156e27e8a351
mamoritz <mats-ola.moritz@sogeti.se>
2016-05-27 16:11:42 +0200

    First version

---
+++ src/paket1/UtvTest.java
@@ -0,0 +1,12 @@
+package paket1;
+
+public class UtvTest {
+
+	public static void main (String args[]){
+		int tuna = 100;
+		int bass = 200;
+		System.out.println(tuna++);
+		System.out.println(tuna);
+		System.out.println(bass);
+	}
+}
