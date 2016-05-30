de432261617dec5e99e7130e5c9cdda76778acbb
mamoritz <mats-ola.moritz@sogeti.se>
2016-05-30 11:25:50 +0200

    First vers 160530

---
+++ Ma30/src/paket1/MaUtskrift.java
@@ -0,0 +1,12 @@
+package paket1;
+
+public class MaUtskrift {
+	public static void main (String args[]){
+		int tuna = 10;
+		int bass = 20;
+		System.out.println(tuna++);
+		System.out.println(tuna);
+		System.out.println(bass);	
+	}
+
+}
