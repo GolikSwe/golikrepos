8fed6265fac7a98ec0db356c7ab06f683389291b
mamoritz <mats-ola.moritz@sogeti.se>
2016-05-30 12:38:09 +0200

    Justering värden i Utskrift

--- Ma30/src/paket1/MaUtskrift.java
+++ Ma30/src/paket1/MaUtskrift.java
@@ -2,8 +2,8 @@ package paket1;
 
 public class MaUtskrift {
 	public static void main (String args[]){
-		int tuna = 10;
-		int bass = 20;
+		int tuna = 100;
+		int bass = 200;
 		System.out.println(tuna++);
 		System.out.println(tuna);
 		System.out.println(bass);	
