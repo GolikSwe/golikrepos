fd0d0f525bebb5fc17e34ccd1e8511b1e4ee8026
mamoritz <mats-ola.moritz@sogeti.se>
2016-05-30 12:50:46 +0200

    Tredje varianten på värden

--- Ma30/src/paket1/MaUtskrift.java
+++ Ma30/src/paket1/MaUtskrift.java
@@ -2,8 +2,8 @@ package paket1;
 
 public class MaUtskrift {
 	public static void main (String args[]){
-		int tuna = 3000;
-		int bass = 4000;
+		int tuna = 10000;
+		int bass = 40000;
 		System.out.println(tuna++);
 		System.out.println(tuna);
 		System.out.println(bass);	
