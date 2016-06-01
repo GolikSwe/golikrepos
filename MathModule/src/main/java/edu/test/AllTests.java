e01f35ebfa5484f0ea3a8d2fc641d8e918e60656
GolikSwe <goranlindqvist14@gmail.com>
2016-06-01 14:15:02 +0200

    junit test suite

---
+++ MathModule/src/main/java/edu/test/AllTests.java
@@ -0,0 +1,11 @@
+package edu.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCalcPrimes.class, TestCalcStat.class })
+public class AllTests {
+
+}
